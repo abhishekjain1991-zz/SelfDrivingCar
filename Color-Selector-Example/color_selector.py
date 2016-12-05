faa6b1f7353940a729f3cba19f08218f9f67cfab
abhjain4 <abhishek50000@gmail.com>
2016-12-05 14:38:39 -0800

    Color-and-Region Selection Code Commit

--- Color-Selector-Example/color_selector.py
+++ Color-Selector-Example/color_selector.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+
 # Read in the image and print out some stats
 image = mpimg.imread('test.jpg')
 print('This image is: ',type(image), 
